7f7fb4977e43f95937cf9f99e41bcebf7bc3c36d
Zach Leslie <zach@puppetlabs.com>
2013-06-12 14:08:43 -0700

    update tests to support :endpoint and :bootable features

--- lib/puppet/type/instance.rb
+++ lib/puppet/type/instance.rb
@@ -1,9 +1,11 @@
 Puppet::Type.newtype(:instance) do
   @doc = "Instance provisioning with puppet."
 
-  feature :endpoint, "The API endpoint to create a connection to."
+  feature :endpoint, "The provider specifies the API endpoint with which to
+  create a connection."
 
-  ensurable
+  feature :bootable, "The provider has a distinction between creating and
+  booting the instance."
 
   newparam(:name, :namevar => true) do
     desc "unique name of instance"
@@ -26,17 +28,33 @@ Puppet::Type.newtype(:instance) do
     desc "What datacenter/region is this instance in?"
   end
 
-  newparam(:image) do
-    isrequired
+  newparam(:image, :required => true) do
+
+    desc "The image to deploy.  What this means depends on the provider.  For
+      example, in Ec2 image referes to an AMI, in Rackspace its the
+      operatingsystem type.  In vSphere, its the path to the template."
+
   end
 
   newparam(:endpoint, :required_features => :endpoint) do
+    desc "The API endpoint to use for communication"
   end
 
   newparam(:insecure, :required_features => :endpoint) do
+    desc "The insecurity level of endpoint"
+
+    newvalues(:true, :false)
+
+    defaultto :false
   end
 
   newparam(:pool) do
+  end
+
+  newproperty(:ensure) do
+    desc("What state the instance should be in.")
+
+    defaultto :present
   end
 
 end
