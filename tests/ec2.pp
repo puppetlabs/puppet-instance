ef852353f32a89a1d4017d4ce377a4ba8a5533a9
Zach Leslie <zach@puppetlabs.com>
2013-07-24 13:21:07 -0700

    update ec2 example

--- tests/ec2.pp
+++ tests/ec2.pp
@@ -1,15 +1,26 @@
+# Creates two instances in EC2
+
+$region = 'us-west-2'
+$zone = 'us-west-2a'
+
+$user = 'xxx'
+$pass = 'xxx'
 
 Instance {
-  provider => 'ec2',
-  image    => 'ami-e030a5d0',
-  flavor   => 't1.micro',
-  user     => "****",
-  pass     => "****",
-  location => "us-west-2",
-  ensure   => absent,
+  ensure     => present,
+  provider   => 'ec2',
+  connection => 'aws-west',
+  image      => 'ami-e030a5d0',
+  flavor     => 't1.micro',
+  location   => $zone,
 }
 
-instance { "test01":
+cloud_connection { 'aws-west':
+  user     => $user,
+  pass     => $pass,
+  location => $region,
 }
-instance { "test02":
-}
+
+instance { "test01": }
+instance { "test02": }
+
